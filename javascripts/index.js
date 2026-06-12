13cbdabe13e05427c6d87cdc8af8e757ef36b9f2
louis <louisxiv831@gmail.com>
2026-06-13 00:26:44 +0200

    ouu shi

---
+++ javascripts/index.js
@@ -0,0 +1,23 @@
+console.log(`Hello, World!`);
+//window.alert(`My name is Monkey d. Luffy! I want to be the Pirate King!`);
+document.getElementById("name").textContent = "Monkey d. Luffy";
+document.getElementById("luffy").textContent = " future Pirate King!";
+document.getElementById("bounty").textContent = " Bounty 3,000,000 berries!";
+document.getElementById("occupation").textContent = " captain of the Straw Hat Pirates!";
+document.getElementById("age").textContent = "age : 19";
+document.getElementById("devil-fruits").textContent = "hito hito no mi model : sun god nika ";
+
+let Ispirateking = true;
+let isThegoat = true;
+if (Ispirateking && isThegoat) {
+console.log(`truth nuke `) 
+}
+
+
+
+
+
+let name = "Monkey d. Luffy"; 
+let bounty = "3,000,000 berries";
+let gmail = "luffy@gmail.com";
+let gmailslice = gmail.slice();
